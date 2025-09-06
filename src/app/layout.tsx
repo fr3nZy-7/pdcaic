2f383949f03adef12b26ee2460df367c2d4b7779
Amey Tambe <amey_iaf@yahoo.co.in>
2025-09-06 19:07:22 +0530

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
