ef723efe071c1effb050beeb3ea608478017a34b
Amey Tambe <amey_iaf@yahoo.co.in>
2025-09-06 17:30:05 +0530

    1st component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import BookAppointmentButton from "./components/common/BookAppointmentButton";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
       <h1 className="text-4xl font-bold">Padmanaabh Dental Clinic</h1>
       <p className="mt-4 text-lg text-gray-600">Welcome to our new website!</p>
       <img src="/images/common/short-logo-wo-name.svg" alt="Clinic Hero" className="mt-6 rounded-lg shadow-lg w-96" />
+      <BookAppointmentButton size="lg"/>
     </main>
   );
 }
